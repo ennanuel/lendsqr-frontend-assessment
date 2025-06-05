f48fe02a77ca22d1c32ccefddbb1e6c0d6ec9173
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-05 09:39:25 +0100

    Added icons
    - Added icons used through out project
    
    - Exported every icon using the 'index.ts' file

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,96 @@
+import arrowDown from "./arrow-down.svg";
+import arrowLeft from "./arrow-left.svg";
+import arrowRight from "./arrow-right.svg";
+import backArrow from "./back-arrow.svg";
+import bank from "./bank.svg";
+import badgePercent1 from "./badge-percent1.svg";
+import briefcase1 from "./briefcase1.svg";
+import briefcase2 from "./briefcase2.svg";
+import calendar from "./calendar.svg";
+import chartBar1 from "./chart-bar1.svg";
+import clipboardList1 from "./clipboard-list1.svg";
+import coinsSolid1 from "./coins-solid1.svg";
+import dropdown1 from "./dropdown1.svg";
+import dropdown2 from "./dropdown2.svg";
+import dropdown3 from "./dropdown3.svg";
+import dropdown4 from "./dropdown4.svg";
+import filter from "./filter.svg";
+import galaxy1 from "./galaxy1.svg";
+import handHolding1 from "./hand-holding1.svg";
+import handshakeRegular1 from "./handshake-regular1.svg";
+import home1 from "./home1.svg";
+import loan1 from "./loan1.svg";
+import loan2 from "./loan2.svg";
+import money1 from "./money1.svg";
+import moreVert from "./more-vert.svg";
+import notification from "./notification.svg";
+import piggyBank1 from "./piggy-bank1.svg";
+import sack1 from "./sack1.svg";
+import savings1 from "./savings1.svg";
+import scroll1 from "./scroll1.svg";
+import search from "./search.svg";
+import slidersH1 from "./sliders-h1.svg";
+import star1 from "./star1.svg";
+import star2 from "./star2.svg";
+import transaction1 from "./transaction1.svg";
+import user1 from "./user1.svg";
+import userCheck1 from "./user-check1.svg";
+import userCheck2 from "./user-check2.svg";
+import userCog1 from "./user-cog1.svg";
+import userDelete from "./user-delete.svg";
+import userFriends1 from "./user-friends1.svg";
+import userTimes1 from "./user-times1.svg";
+import users1 from "./users1.svg";
+import users2 from "./users2.svg";
+import users3 from "./users3.svg";
+import view1 from "./view1.svg";
+
+
+export {
+    arrowDown,
+    arrowLeft,
+    arrowRight,
+    backArrow,
+    bank,
+    badgePercent1,
+    briefcase1,
+    briefcase2,
+    calendar,
+    chartBar1,
+    clipboardList1,
+    coinsSolid1,
+    dropdown1,
+    dropdown2,
+    dropdown3,
+    dropdown4,
+    filter,
+    galaxy1,
+    handHolding1,
+    handshakeRegular1,
+    home1,
+    loan1,
+    loan2,
+    money1,
+    moreVert,
+    notification,
+    piggyBank1,
+    sack1,
+    savings1,
+    scroll1,
+    search,
+    slidersH1,
+    star1,
+    star2,
+    transaction1,
+    user1,
+    userCheck1,
+    userCheck2,
+    userCog1,
+    userDelete,
+    userFriends1,
+    userTimes1,
+    users1,
+    users2,
+    users3,
+    view1
+}
