8ff3b37df7cc626c6818b25874edc847438b34cf
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-05 10:14:56 +0100

    Refactored 'options' component
    - Imported 'UserOptions' component to make code more readable
    
    - Changed logic of 'openOptionsModal' function to take 'id' as argument instead of 'index'

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -4,30 +4,27 @@ import { UsersProps } from "@/types/index.types";
 
 import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
-import Image from "next/image";
-
-import { USER_OPTIONS } from "@/assets/data/user";
-import { moreVert } from "@/assets/icons";
 
 import { changeDateFormat } from "@/helpers";
+import UserOptions from "./UserOptions";
 
 export default function UsersList({ users }: UsersProps) {
     const router = useRouter();
     const optionsContainerRef = useRef<HTMLUListElement>(null);
-    const [indexOfUserOptionToShow, setIndexOfUserOptionToShow] = useState<number | null>(null);
+    const [idOfUserOptionToShow, setIdOfUserOptionToShow] = useState<string | null>(null);
     
-    function openOptionsModal(index: number) {
-        setIndexOfUserOptionToShow(index);
+    function openOptionsModal(userId: string) {
+        setIdOfUserOptionToShow(userId);
     };
     function closeOptionsModal() {
-        setIndexOfUserOptionToShow(null);
+        setIdOfUserOptionToShow(null);
     };
     function navigateToUser(userId: string) {
         router.push(`/user/${userId}`);
     };
 
     const handleModalClick: React.MouseEventHandler<HTMLDivElement> = (event) => {
-        if (!optionsContainerRef.current || indexOfUserOptionToShow === null) return;
+        if (!optionsContainerRef.current || idOfUserOptionToShow === null) return;
 
         const containerRect = optionsContainerRef.current.getBoundingClientRect();
         const clickedOutsideModalRange = event.clientX < containerRect.left ||
@@ -52,26 +49,13 @@ export default function UsersList({ users }: UsersProps) {
                             <span className={`user-status ${status.toLowerCase()}`}>{status.toLowerCase()}</span>
                         </td>
                         <td className="more-column">
-                            <span className="button-and-options-container">
-                                <button onClick={() => openOptionsModal(index)} className="more-btn">
-                                    <Image src={moreVert} className="more-icon" alt="More icon" width={20} height={20} />
-                                </button>
-
-                                <div onClick={handleModalClick} className={`options-container ${indexOfUserOptionToShow == index ? 'show-menu' : 'hide'}`}>
-                                    <ul ref={optionsContainerRef}>
-                                        {
-                                            USER_OPTIONS.map(({ title, icon }) => (
-                                                <li key={title}>
-                                                    <button className="option">
-                                                        <Image src={icon} alt={`${title} icon`} width={14} height={14} className="option-icon" />
-                                                        <span className="font-work-sans">{title}</span>
-                                                    </button>
-                                                </li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                            </span>
+                            <UserOptions
+                                id={_id}
+                                idOfUserOptionToShow={idOfUserOptionToShow}
+                                openOptionsModal={openOptionsModal}
+                                handleModalClick={handleModalClick} 
+                                optionsContainerRef={optionsContainerRef}
+                            />
                         </td>
                     </tr>
                 ))
