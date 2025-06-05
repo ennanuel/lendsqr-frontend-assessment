ab5ccd5ca521c7d6b5a7e98f2cb32e1c089bc65d
Emmanuel Ezema <emmanuelzema6@gmail.com>
2025-06-05 09:32:51 +0100

    Added file that contains dashboard data
    - File exports constants that will be used in dashboard page

---
+++ src/assets/data/dashboardStats.ts
@@ -0,0 +1,29 @@
+import { loan2, money1, users2, users3 } from "../icons";
+
+
+export const DASHBOARD_STATS = [
+    {
+        title: "Users",
+        icon: users2,
+        stat: "2,453",
+        color: "purple"
+    },
+    {
+        title: "Active users",
+        icon: users3,
+        stat: "2,453",
+        color: "blue"
+    },
+    {
+        title: "Users with loans",
+        icon: loan2,
+        stat: "12,453",
+        color: "orange"
+    },
+    {
+        title: "Users with savings",
+        icon: money1,
+        stat: "102,453",
+        color: "red"
+    },
+];
